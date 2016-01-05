4a244eb374045768333ac57fa5f80d8182038423
Gary Reynolds <gary@touch.asn.au>
2016-01-06 08:32:36 +1100

    Add CompatibleCeleryEmailBackend

--- djcelery_email/backends.py
+++ djcelery_email/backends.py
@@ -16,3 +16,26 @@ class CeleryEmailBackend(BaseEmailBackend):
         for chunk in chunked(messages, settings.CELERY_EMAIL_CHUNK_SIZE):
             result_tasks.append(send_emails.delay(chunk, self.init_kwargs))
         return result_tasks
+
+
+class CompatibleCeleryEmailBackend(CeleryEmailBackend):
+    """
+    ``BaseEmailBackend.send_messages`` does not impose a return type so we have
+    a conundrum. Should we use this to our advantage and return anything
+    (because there is no spec disallowing it)?
+
+    Alternatively we could behave like the bundled backends such as
+    ``django.core.mail.backends.smtp.EmailBackend``.
+
+    This implementation attempts to act like the latter.
+
+    Underneath we still use the ``CeleryEmailBackend`` but return the count.
+    """
+    def send_messages(self, email_messages):
+        if not email_messages:
+            return
+        async_results = (
+            super(CompatibleCeleryEmailBackend, self)
+            .send_messages(email_messages)
+        )
+        return len(async_results)
